3e8812615438dc6c157e0d56634d6bd499303862
Nirudasss <swamibaba079@gmail.com>
2026-01-26 19:00:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = os.environ.get("API_HASH", "527e6297989f4e7cda5091f5bf41d0e4")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "7737048829").split(",")]
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://dasniru929:dasniru123@cluster0.51p5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "EliteSaveRestricted")
-LOG_CHANNEL = os environ.get("LOG_CHANNEL", "-1003592723606")
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1003592723606")
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
 # Rexbots
