d0123b3307f9d8d79a7356ba1e8620ceb9c15c0d
Nirudasss <swamibaba079@gmail.com>
2026-01-26 17:53:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@
 # Telegram Channel @RexBots_Official
 import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "")
-LOG_CHANNEL = -1003656791142
+API_ID = int(os.environ.get("API_ID", "10660564"))
+API_HASH = os.environ.get("API_HASH", "527e6297989f4e7cda5091f5bf41d0e4")
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "7737048829").split(",")]
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://dasniru929:dasniru123@cluster0.51p5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "EliteSaveRestricted")
+LOG_CHANNEL = os environ.get("LOG_CHANNEL", "-1003592723606")
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
 # Rexbots
